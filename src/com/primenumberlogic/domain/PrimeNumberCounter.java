0ba6ec6330b50b809b7472042988211afc982ec4
Ash-girl Chapfuwa <ashgirlchapfuwa@gmail.com>
2015-01-30 01:36:35 -0300

    use immutable list

--- src/com/primenumberlogic/domain/PrimeNumberCounter.java
+++ src/com/primenumberlogic/domain/PrimeNumberCounter.java
@@ -1,33 +1,32 @@
 package com.primenumberlogic.domain;
 
-import java.util.ArrayList;
+import com.google.common.collect.ImmutableList;
+
 import java.util.List;
 
 public class PrimeNumberCounter {
 
     public int count(int maxNumber) {
 
-        return incrementCount(maxNumber, maxNumber, new ArrayList<Integer>());
+        return incrementCount(maxNumber, maxNumber, new ImmutableList.Builder<Integer>().build());
 
     }
 
-    private int incrementCount(int maxNumber, int currentNumber, List<Integer> primeNumbers) {
-
-        List<Integer> newList = new ArrayList<Integer>();
-        newList.addAll(primeNumbers);
+    private int incrementCount(int maxNumber, int currentNumber, ImmutableList<Integer> primeNumbers) {
 
         int diff = maxNumber - currentNumber;
 
         if (currentNumber == 0) {
-            return isPrime(diff, newList) ? primeNumbers.size() + 1 : primeNumbers.size();
+            return isPrime(diff, primeNumbers) ? primeNumbers.size() + 1 : primeNumbers.size();
         }
 
         if (isPrime(diff, primeNumbers)) {
-            newList.add(diff);
-            return incrementCount(maxNumber, currentNumber - 1, newList);
+            final ImmutableList<Integer> newPrimeNumbers = new ImmutableList.Builder<Integer>().addAll(primeNumbers).add(diff).build();
+            return incrementCount(maxNumber, currentNumber - 1, newPrimeNumbers);
         }
 
-        return incrementCount(maxNumber, currentNumber - 1, newList);
+
+        return incrementCount(maxNumber, currentNumber - 1, primeNumbers);
     }
 
     private boolean isPrime(int number, List<Integer> primeNumbers) {
